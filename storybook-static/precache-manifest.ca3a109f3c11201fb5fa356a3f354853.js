afad889016b2ba451353a64eefe99bc3ba78ecf7
John Ragan <john.ragan@gmail.com>
2020-08-24 14:27:24 -0400

    Added support for publishing and deploying automatic changes via CI to chromatic.

---
+++ storybook-static/precache-manifest.ca3a109f3c11201fb5fa356a3f354853.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8746aca3c2467e69b0064a4f7dd6cf8",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.cec240668e574e2bf7cb.bundle.js"
+  },
+  {
+    "url": "runtime~main.cec240668e574e2bf7cb.bundle.js"
+  },
+  {
+    "revision": "52ce7704fab603ba8b65",
+    "url": "static/css/main.b15adadd.chunk.css"
+  },
+  {
+    "url": "vendors~main.cec240668e574e2bf7cb.bundle.js"
+  },
+  {
+    "url": "vendors~main.cec240668e574e2bf7cb.bundle.js.LICENSE.txt"
+  }
+]);
